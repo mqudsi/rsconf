0b639693db50916013388b838b609b438ed2aa7e
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-05-11 17:15:24 -0500

    Remove dropped version of snippets

--- snippets/symbol_int_value.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <{0}>
-
-int main() {{
-	fprintf(stdout, "%u", (uint32_t)({1}));
-	return 0;
-}}
--- snippets/symbol_long_value.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <{0}>
-
-int main() {{
-	fprintf(stdout, "%llu", (uint64_t)({1}));
-	return 0;
-}}
