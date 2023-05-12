e6001a1f789841beca6b045515d7712b60eb6664
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-05-12 16:33:41 -0500

    Fix Detector.if() under MSVC
    It turns out that MSVC errors out on the word "INVALID" - even when it's in a
    section #ifdef'd out entirely!

--- snippets/if.c
+++ snippets/if.c
@@ -4,6 +4,8 @@ int main() {{
 #if {1}
 	return 0;
 #else
-	INVALID@INVALID
+	// INVALID@INVALID
+	// It turns out INVALID is a reserved keyword under MSVC, even when it's #if'd out!
+	FOO17@BAR24
 #endif
 }}
