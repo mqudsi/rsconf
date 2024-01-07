c2051e7b237006dadc6066fdd8ddec0960681dc4
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2024-01-07 13:08:03 -0600

    ifdef.c: Don't use INVALID which is reserved under MSVC

--- snippets/ifdef.c
+++ snippets/ifdef.c
@@ -5,6 +5,8 @@ int main() {{
 #if defined({1})
 	return 0;
 #else
-	INVALID@INVALID
+	// INVALID@INVALID
+	// It turns out INVALID is a reserved keyword under MSVC, even when it's #if'd out!
+	FOO17@BAR24
 #endif
 }}
